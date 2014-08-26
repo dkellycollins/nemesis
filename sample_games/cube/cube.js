f31c4eefde53ad385a0e24e2a6c2d70196057957
Devin Kelly-Collins <dkellycollins@gmail.com>
2014-08-26 15:07:20 -0700

    Removed submdules. Removed tyescript.

---
+++ sample_games/cube/cube.js
@@ -0,0 +1,11 @@
+$(function () {
+    var canvas = $('cube-canvas');
+    initGL(canvas);
+    initShaders();
+    initBuffers();
+
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.enable(gl.DEPTH_TEST);
+
+    drawScene();
+})
---
+++ test/ArrayTest.js
@@ -0,0 +1,13 @@
+///<reference path="../lib/mocha/mocha.d.ts" />
+///<reference path="../lib/requirejs/require.d.ts" />
+var assert = require("assert");
+
+describe('Array', function () {
+    describe('#indexOf()', function () {
+        it('returns -1 when the value is not present', function () {
+            assert.equal(-1, [1, 2, 3].indexOf(5));
+            assert.equal(-1, [1, 2, 3].indexOf(0));
+        });
+    });
+});
+//# sourceMappingURL=ArrayTest.js.map
