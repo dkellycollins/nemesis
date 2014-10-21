64d4047ec9c07449af9dd617101617c8a077384f
Devin Kelly-Collins <dkellycollins@gmail.com>
2014-10-21 15:43:52 -0500

    Fixed issue with declaring nemesisConfig.

--- src/nemesisConfig.d.ts
+++ src/nemesisConfig.d.ts
@@ -1,2 +1,27 @@
-declare module 'json!config.json' {}
+declare module 'json!config.json' {
+    /*
+     * The id of the canvas element in the html page. If not provide the fist canvas element on the page will be used.
+     */
+    export var canvasId: string;
+
+    /*
+     * If true the canvas element will be resized to fill the window and keep the size of the window.
+     */
+    export var fullscreen: boolean;
+
+    /*
+     * If true, an error will be thrown when WebGL encounters an eror.
+     */
+    export var throwOnGLError: boolean;
+
+    /*
+     * If true, will log all calls to web gl.
+     */
+    export var logGLCalls: boolean;
+
+    /*
+     * If true, calls to web gl will be checked for undefined parameters. Any calls with undefined parameters will be logged.
+     */
+    export var validateGLArgs: boolean;
+}
 
--- src/nemesisConfig.ts
+++ src/nemesisConfig.ts
@@ -4,34 +4,7 @@
 import config = require("json!config.json");
 import _ = require("lodash");
 
-interface NemesisConfig {
-    /*
-     * The id of the canvas element in the html page. If not provide the fist canvas element on the page will be used.
-     */
-    canvasId: string;
-
-    /*
-     * If true the canvas element will be resized to fill the window and keep the size of the window.
-     */
-    fullscreen: boolean;
-
-    /*
-     * If true, an error will be thrown when WebGL encounters an eror.
-     */
-    throwOnGLError: boolean;
-
-    /*
-     * If true, will log all calls to web gl.
-     */
-    logGLCalls: boolean;
-
-    /*
-     * If true, calls to web gl will be checked for undefined parameters. Any calls with undefined parameters will be logged.
-     */
-    validateGLArgs: boolean;
-}
-
-_.defaults(config, <NemesisConfig>{
+_.defaults(config, {
     canvasId: "",
     fullscreen: false,
     throwOnGLError: true,
