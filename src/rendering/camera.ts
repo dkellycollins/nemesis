bd8081880dc1da580e786a33cb98fa9f633a1399
Devin Kelly-Collins <dkellycollins@gmail.com>
2014-10-30 21:04:59 -0500

    Added projection and view matrices to the camera.

--- src/rendering/camera.ts
+++ src/rendering/camera.ts
@@ -2,55 +2,32 @@ import vec3 = require("../util/math/vec3");
 import mat4 = require("../util/math/mat4");
 
 class camera {
-    constructor() {
-        this.orientation = vec3.fromValues(Math.PI, 0, 0);
-        this.position = vec3.create();
+    constructor(pos?, target?) {
+        this._pos = pos || vec3.create();
+        this._target = target || vec3.create();
+        this._proj = mat4.create();
     }
 
-    public orientation;
-    public position;
+    private _pos;
+    private _target;
+    private _proj;
 
-    public getViewMatrix() {
-        var lookat = vec3.fromValues(
-            Math.sin(this.orientation[0]) * Math.cos(this.orientation[1]),
-            Math.sin(this.orientation[1]),
-            Math.cos(this.orientation[0]) * Math.cos(this.orientation[2]));
-        vec3.add(lookat, this.position, lookat);
-        return mat4.lookAt(mat4.create(), this.position, lookat, vec3.UNIT_Y);
+    public getProjection() {
+        return mat4.copy(mat4.create(), this._proj);
     }
 
-    public move(x:number, y:number, z:number, speed:number):void {
-        var forward = vec3.fromValues(
-            Math.sin(this.orientation[0]),
-            0,
-            Math.cos(this.orientation[0]));
-        var right = vec3.fromValues(
-            -forward[2],
-            0,
-            forward[2]);
-        var offset = vec3.create();
-        var o = vec3.create();
-        vec3.add(offset, offset, vec3.scale(o, right, x));
-        vec3.add(offset, offset, vec3.scale(o, forward, y));
-        offset[1] += z;
+    public getView() {
+        return mat4.lookAt(mat4.create(), this._pos, this._target, vec3.UNIT_Y);
+    }
 
-        vec3.normalize(offset, offset);
-        vec3.scale(offset, offset, speed);
-        vec3.add(this.position, this.position, offset);
+    public setPerspective(fovy:number, aspect:number, near:number, far:number):void {
+        mat4.perspective(this._proj, fovy, aspect, near, far);
     }
 
     public reset():void {
-        vec3.set(this.position, 0, 0, 0);
-        vec3.set(this.orientation, Math.PI, 0, 0);
-    }
-
-    public rotate(x:number, y:number):void {
-        this.orientation[0] = (this.orientation[0] + x) % (Math.PI * 2);
-        this.orientation[1] = Math.max(
-            Math.min(
-                this.orientation[1] + y,
-                (Math.PI / 2 - 0.1)),
-            -Math.PI / 2 + 0.1);
+        vec3.set(this._pos, 0, 0, 0);
+        vec3.set(this._target, 0, 0, 0);
+        mat4.identity(this._proj);
     }
 }
-export = camera;
+export = camera
