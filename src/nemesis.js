02e163c95dc2727f1b461e2608f3ff9fc2c752d4
Devin Kelly-Collins <dkellycollins@gmail.com>
2014-10-30 21:01:55 -0500

    Added update and render call backs to be called in the animation loop.

--- src/nemesis.js
+++ src/nemesis.js
@@ -1,25 +1,49 @@
 define(["require", "exports"], function(require, exports) {
     /*import Rendering = require("./rendering/rendering");
     import Util = require("./util/util");*/
+    var _nemesis;
+
     /* The nemesis module is for static varibles and static initialization. */
     var nemesis;
     (function (nemesis) {
         //export var rendering = Rendering;
         //export var util = Util;
-        var _animate;
-        ;
-        function animate(animateFunc, args) {
-            _animate = function (time, args) {
-                animateFunc(time, args);
-                window.requestAnimationFrame(function (t) {
-                    _animate(t, args);
-                });
-            };
-            window.requestAnimationFrame(function (t) {
-                _animate(t, args);
-            });
+        var _updateCallbacks = [];
+        var _renderCallbacks = [];
+
+        function registerUpdateCallback(update) {
+            return _updateCallbacks.push(update);
         }
-        nemesis.animate = animate;
+        nemesis.registerUpdateCallback = registerUpdateCallback;
+
+        function deregisterUpdateCallback(updateIndex) {
+            _updateCallbacks.splice(updateIndex, 1);
+        }
+        nemesis.deregisterUpdateCallback = deregisterUpdateCallback;
+
+        function registerRenderCallback(render) {
+            return _renderCallbacks.push(render);
+        }
+        nemesis.registerRenderCallback = registerRenderCallback;
+
+        function deregisterRenderCallback(renderIndex) {
+            _renderCallbacks.splice(renderIndex, 1);
+        }
+        nemesis.deregisterRenderCallback = deregisterRenderCallback;
+
+        function run(context) {
+            var animateFrame = function (time) {
+                _updateCallbacks.forEach(function (callback) {
+                    callback(time, context);
+                });
+                _renderCallbacks.forEach(function (callback) {
+                    callback(time, context);
+                });
+                window.requestAnimationFrame(animateFrame);
+            };
+            window.requestAnimationFrame(animateFrame);
+        }
+        nemesis.run = run;
     })(nemesis || (nemesis = {}));
     
     return nemesis;
--- src/nemesis.ts
+++ src/nemesis.ts
@@ -2,22 +2,43 @@ import Logger = require("./util/logging/consoleLogger");
 /*import Rendering = require("./rendering/rendering");
 import Util = require("./util/util");*/
 
+var _nemesis;
+
 /* The nemesis module is for static varibles and static initialization. */
 module nemesis {
     //export var rendering = Rendering;
     //export var util = Util;
 
-    var _animate:(time: any, args:any) => void;;
-    export function animate(animateFunc: (time: any, args:any) => void, args?: any) {
-        _animate = (time, args:any) => {
-            animateFunc(time, args);
-            window.requestAnimationFrame((t) => {
-                _animate(t, args)
+    var _updateCallbacks: any[] = [];
+    var _renderCallbacks: any[] = [];
+
+    export function registerUpdateCallback(update: (time:any, context: any) => void):number {
+        return _updateCallbacks.push(update);
+    }
+
+    export function deregisterUpdateCallback(updateIndex: number):void {
+        _updateCallbacks.splice(updateIndex, 1);
+    }
+
+    export function registerRenderCallback(render: (time:any, context: any) => void): number {
+        return _renderCallbacks.push(render);
+    }
+
+    export function deregisterRenderCallback(renderIndex: number):void {
+        _renderCallbacks.splice(renderIndex, 1);
+    }
+
+    export function run(context?:any):void {
+        var animateFrame = (time) => {
+            _updateCallbacks.forEach(callback => {
+                callback(time, context);
             });
+            _renderCallbacks.forEach(callback => {
+                callback(time, context);
+            });
+            window.requestAnimationFrame(animateFrame);
         };
-        window.requestAnimationFrame((t) => {
-            _animate(t, args)
-        });
+        window.requestAnimationFrame(animateFrame);
     }
 }
 export = nemesis;
