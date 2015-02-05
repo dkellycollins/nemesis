088cab86665fdde8c1bb681d9c8612f9fc048119
Devin Kelly-Collins <dkellycollins@gmail.com>
2015-02-04 20:19:32 -0600

    Removed javascript files.

--- src/engine/input/index.js
+++
@@ -1,5 +0,0 @@
-define(["require", "exports", './keyboard', './mouse', './mouseButtons'], function (require, exports, keyboard_file, mouse_file, mouseButtons_file) {
-    exports.keyboard = keyboard_file; ///ts:export:generated
-    exports.mouse = mouse_file; ///ts:export:generated
-    exports.mouseButtons = mouseButtons_file; ///ts:export:generated
-});
--- src/engine/rendering/index.js
+++
@@ -1,8 +0,0 @@
-define(["require", "exports", './glContext', './shaders', './render', './camera', './texture', './renderObject'], function (require, exports, GL_file, shaders_file, render_file, camera_file, texture_file, renderObject_file) {
-    exports.GL = GL_file; ///ts:export:generated
-    exports.shaders = shaders_file; ///ts:export:generated
-    exports.render = render_file; ///ts:export:generated
-    exports.camera = camera_file; ///ts:export:generated
-    exports.texture = texture_file; ///ts:export:generated
-    exports.renderObject = renderObject_file; ///ts:export:generated
-});
