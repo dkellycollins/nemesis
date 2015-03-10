70ba93e3802e9a94c55a2304623fe92c2f469deb
Devin Kelly-Collins <devin.kellycollins@beyondthescores.com>
2015-03-10 09:11:54 -0500

    Updated canvas logic

--- src/nemesis/canvas.ts
+++ src/nemesis/canvas.ts
@@ -1,29 +1,37 @@
 module nemesis {
-    import logger = nemesis.logger;
+    import nemesis = nemesis;
 
-    /**
-     * The canvas element on the page.
-     * Will retrieve any element with a nemesis attribute or just the first canvas element on the page.
-     */
-    export var canvas;
+    var _canvas;
 
-    var elements = document.getElementsByTagName('canvas');
-
-    if(elements.length == 0) {
-        logger.logError('No canvas elements found.');
-    } else {
-        for(var i = 0; i < elements.length; i++) {
-            if(elements[i].hasAttribute('nemesis')) {
-                canvas = elements[i];
-                break;
-            }
-        }
-        canvas = canvas || elements[0];
-
-        //Fullscreen option must be handled here
-        if(canvas.getAttribute("fullscreen") == "true") {
-            canvas.height = window.innerHeight;
-            canvas.width = window.innerWidth;
-        }
+   /**
+    * The canvas element on the page.
+    * Will retrieve any element with a nemesis attribute or just the first canvas element on the page.
+    */
+    export function canvas() {
+       return _canvas;
     }
+
+    var initCanvas = () => {
+       var elements = document.getElementsByTagName('canvas');
+
+       if(elements.length == 0) {
+          logger.logError('No canvas elements found.');
+       } else {
+          for(var i = 0; i < elements.length; i++) {
+             if(elements[i].hasAttribute('nemesis')) {
+                _canvas = elements[i];
+                break;
+             }
+          }
+          _canvas = _canvas || elements[0];
+
+          //Fullscreen option must be handled here
+          if(_canvas.getAttribute("fullscreen") == "true") {
+             _canvas.height = window.innerHeight;
+             _canvas.width = window.innerWidth;
+          }
+       }
+    };
+
+    nemesis.on("init", initCanvas);
 }
